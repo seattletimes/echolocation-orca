e035d271b8a9ed664b7b3a1b417f55dc7255a6d8
emilymeng <eeng@seattletimes.com>
2019-05-17 00:26:52 -0700

    buttons working

--- src/js/main.js
+++ src/js/main.js
@@ -11,10 +11,26 @@ var qsa = s => Array.prototype.slice.call(document.querySelectorAll(s));
 
 qsa(".st-group").forEach(function(group) {
   group.addEventListener("click", function(e) {
-    document.querySelector(".info").innerHTML = template(data[e.target.parentElement.id]);
+    document.querySelector(".details").innerHTML = template(data[e.target.parentElement.id]);
     if (document.querySelector(".selected")) document.querySelector(".selected").classList.remove("selected");
     e.target.parentElement.classList.add("selected");
   });
+});
 
-  
+var options = Object.keys(window.data);
+
+qsa(".goto").forEach(function(btn) {
+	btn.addEventListener("click", function(e) {
+		var selected = document.querySelector(".selected").id;
+		var selectedIndex = selected ? options.indexOf(selected) : -1;
+		if (e.target.classList.contains("back")) {
+			selectedIndex = Math.max(0, selectedIndex - 1);
+		} else {
+			selectedIndex = Math.min(options.length - 1, selectedIndex + 1);
+		}
+
+		document.querySelector(".details").innerHTML = template(data[options[selectedIndex]]);
+    if (document.querySelector(".selected")) document.querySelector(".selected").classList.remove("selected");
+    document.querySelector("#" + options[selectedIndex]).classList.add("selected");
+	});
 });
