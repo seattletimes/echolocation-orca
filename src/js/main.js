310db2ef7e5ddc7fa2e7873cca7fec72fed16131
emilymeng <eeng@seattletimes.com>
2019-05-17 01:00:13 -0700

    final css

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@
 
 require("component-responsive-frame/child");
 var dot = require("./lib/dot");
+var $ = require("jquery");
 
 var template = dot.compile(require("./_info.html"));
 
@@ -21,16 +22,22 @@ var options = Object.keys(window.data);
 
 qsa(".goto").forEach(function(btn) {
 	btn.addEventListener("click", function(e) {
-		var selected = document.querySelector(".selected").id;
-		var selectedIndex = selected ? options.indexOf(selected) : -1;
+		var selected = document.querySelector(".selected");
+
+		var selectedIndex = selected ? options.indexOf(selected.id) : -1;
 		if (e.target.classList.contains("back")) {
 			selectedIndex = Math.max(0, selectedIndex - 1);
 		} else {
 			selectedIndex = Math.min(options.length - 1, selectedIndex + 1);
 		}
 
+		$(".goto.back").toggleClass("disabled", selectedIndex == 0);
+    $(".goto.next").toggleClass("disabled", selectedIndex == options.length - 1);
+
 		document.querySelector(".details").innerHTML = template(data[options[selectedIndex]]);
     if (document.querySelector(".selected")) document.querySelector(".selected").classList.remove("selected");
     document.querySelector("#" + options[selectedIndex]).classList.add("selected");
 	});
 });
+
+
